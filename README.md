d56aa55988318865f73a421a1baf3dca5017a47d
coding-spidey <rishabhgoswami938@gmail.com>
2021-05-24 15:06:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Based App having a custom http hook
+
+A React based SPA in which I have created a custom hook for sending and getting http requests. Using this hook, you can both send data to your backend server as well as fetch data from your API.
+
+## How To Run
+
+- Fork the repository
+- Add variable `REACT_APP_URL` in a your .env file, it is the url to your backend API.
+- run `npm i` in the command terminal.
+- run `npm start` and the application will start on `localhost:3000`.
---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,38 @@
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const addTaskHandler = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: process.env.REACT_APP_URL,
+        method: "POST",
+        body: taskText,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      addTaskHandler.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
